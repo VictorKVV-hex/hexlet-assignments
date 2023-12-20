a3fd88a00daa3c89550a6f4acf54b65b8827c795
@hexlet/cli <support@hexlet.io>
2023-12-20 23:11:33 +0300

    submit java-oop-ru/interfaces

---
+++ java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
@@ -0,0 +1,26 @@
+package exercise;
+
+public class Cottage implements Home{
+    double area;
+    int floorCount;
+
+    public Cottage(double area, int floorCount) {
+        this.area = area;
+        this.floorCount = floorCount;
+    }
+
+    @Override
+    public double getArea() {
+        return area;
+    }
+    @Override
+    public int compareTo(Home another) {
+        int compare = Double.compare(area, another.getArea());
+        return Double.compare(area, another.getArea());
+    }
+
+    @Override
+    public String toString() {
+        return floorCount + " этажный коттедж площадью " + area + " метров";
+    }
+}
---
+++ java-oop-ru/interfaces/src/main/java/exercise/Flat.java
@@ -0,0 +1,28 @@
+package exercise;
+
+public class Flat implements Home {
+    double area;
+    double balconyArea;
+    int floor;
+    public Flat (double area, double balconyArea, int floor) {
+        this.area = area;
+        this.balconyArea = balconyArea;
+        this.floor = floor;
+    }
+    @Override
+    public double getArea() {
+        return area + balconyArea;
+    }
+    @Override
+    public int compareTo(Home another) {
+//        return (area < another.getArea()) ? -1 : ((area == another.getArea()) ? 0 : 1);
+        int compare = Double.compare(area, another.getArea());
+        return Double.compare((area + balconyArea), another.getArea());
+    }
+
+    @Override
+    public String toString() {
+        return "Квартира площадью " + (area+balconyArea) + " метров на " + floor + " этаже";
+    }
+}
+
