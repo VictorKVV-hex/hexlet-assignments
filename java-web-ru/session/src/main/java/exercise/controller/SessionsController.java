d3e2f769a58be33b4d831edf14b7679b65780ec7
@hexlet/cli <support@hexlet.io>
2024-03-25 23:51:23 +0300

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -1,25 +1,25 @@
 package exercise.controller;
 
-import static io.javalin.rendering.template.TemplateUtil.model;
+import java.util.Collections;
 import exercise.dto.MainPage;
 import exercise.dto.LoginPage;
 import exercise.repository.UsersRepository;
-import static exercise.util.Security.encrypt;
 import exercise.util.NamedRoutes;
 import io.javalin.http.Context;
 
-import java.util.Collections;
+import static exercise.util.Security.encrypt;
+
 
 public class SessionsController {
 
     // BEGIN
     public static void loginPage(Context ctx) {
         var page = new LoginPage("", null);
-        ctx.render("build.jte", model("page", page));
+        ctx.render("build.jte", Collections.singletonMap("page", page));
     }
 
     public static void login(Context ctx) {
-        var username = ctx.formParam("username");
+        var username = ctx.formParam("name");
         var password = ctx.formParam("password");
         var user = UsersRepository.findByName(username);
 
