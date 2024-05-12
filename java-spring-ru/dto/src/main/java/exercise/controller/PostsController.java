69aeb174f53638d703ece81c84ac31135d126e1c
@hexlet/cli <support@hexlet.io>
2024-05-12 21:40:08 +0300

    submit java-spring-ru/dto

--- java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -1,9 +1,12 @@
 package exercise.controller;
 
+import exercise.model.Comment;
 import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
@@ -15,5 +18,50 @@ import exercise.dto.PostDTO;
 import exercise.dto.CommentDTO;
 
 // BEGIN
+@RestController
+@RequestMapping(path = "/posts")
+public class PostsController {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
+    private CommentDTO commentDTO(Comment comment) {
+        var dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setBody(comment.getBody());
+        return dto;
+    }
+    private PostDTO postToDTO(Post post) {
+        var dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setBody(post.getBody());
+        var comments = commentRepository.findByPostId(post.getId());
+        var dtoComments = comments.stream()
+                .map(comment -> commentDTO(comment))
+                .toList();
+        dto.setComments(dtoComments);
+        return dto;
+    }
+
+    @GetMapping(path = "")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PostDTO> index() {
+        var posts = postRepository.findAll();
+        return posts.stream()
+//                .map(this::postToDTO)
+//                .map(post -> this.postToDTO(post))
+                .map(post -> postToDTO(post))
+                .toList();
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PostDTO show(@PathVariable Long id) {
+        var post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+        return postToDTO(post);
+    }
+}
 // END
--- java-spring-ru/dto/src/main/java/exercise/dto/PostDTO.java
+++ java-spring-ru/dto/src/main/java/exercise/dto/PostDTO.java
@@ -5,5 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 // BEGIN
-
+@Setter
+@Getter
+public class PostDTO {
+    private Long id;
+    private String title;
+    private String body;
+    private List<CommentDTO> comments;
+}
 // END
